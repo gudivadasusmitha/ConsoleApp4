19cf93410fdcc94895f5f24fe9c6f52749fc38e1
gudivadasusmitha <Gudivada.Susmitha@ibm.com>
2022-03-01 14:26:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //program to get the employee details and display them
+            string name, add, city;
+            int empID, age;
+            Console.Write("name: ");
+            name = Console.ReadLine();
+            empID = int.Parse(Console.ReadLine());
+            Console.Write("address: ");
+            add = Console.ReadLine();
+            Console.Write("city: ");
+            city = Console.ReadLine();
+            Console.Write("age: ");
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("the details of {0}:", name);
+            Console.WriteLine("employee id :" + empID);
+            Console.WriteLine("age :" + add);
+            Console.WriteLine("city:" + city);
+            Console.ReadLine();
+            
+
+            //n natural numbers
+
+        }
+    }
+}
